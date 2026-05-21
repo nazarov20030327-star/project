316b574ac09969517e0fcc9421e2bc501510dd91
nazar <neki_2017@bk.ru>
2026-05-21 14:39:07 +0300

    init

---
+++ src/cart.py
@@ -0,0 +1,31 @@
+# Список товаров, которые купил покупатель
+cart = ["apple", "bread", "milk", "apple", "chocolate"]
+
+# Словарь с ценами (цена за 1 штуку)
+prices = {
+    "apple": 50,
+    "bread": 30,
+    "milk": 80,
+    "chocolate": 120
+}
+total_price = 0
+seen_items = {}
+
+for item in cart:
+    if item in seen_items:
+        seen_items[item] += 1
+    else:
+        seen_items[item] = 1
+    #проверяем какой по счету товар
+    if seen_items[item] == 1:
+        total_price += prices[item]
+    else:
+        total_price += prices[item]/2
+
+print(total_price)
+
+
+
+
+
+
--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-from src.masks import get_mask_card_number, get_mask_account
-
-
+from masks import get_mask_card_number, get_mask_account
 def mask_account_card(card_string: str) -> str:
     """
     Маскирует номер карты или счёта из одной строки.
